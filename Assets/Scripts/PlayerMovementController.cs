0dffd4a6d86b7e3b1a8cbd6df49f54dd3e2f9776
Sameerstg <sameerstg@outlook.com>
2023-01-15 21:39:19 +0500

    Network lag fixed

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -4,8 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovementController : MonoBehaviour,IPunObservable
-{
+public class PlayerMovementController : MonoBehaviour{
     protected Rigidbody2D rb;
     
     public float moveSpeed;
@@ -38,7 +37,8 @@ public class PlayerMovementController : MonoBehaviour,IPunObservable
     }
     private void Update()
     {
-        
+       
+
         if (rb.velocity.SqrMagnitude() >= moveMaxSpeed && photonView.IsMine)
         {
             rb.velocity = new Vector2(Vector2.ClampMagnitude(rb.velocity, moveMaxSpeed).x,rb.velocity.y);
@@ -80,29 +80,5 @@ public class PlayerMovementController : MonoBehaviour,IPunObservable
         
     }
     internal void SetMoveDirection(Vector2 direction)=>this.direction= direction;
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(transform.position);
-        }
-        else
-        {
-            remotePosition = (Vector2)stream.ReceiveNext();
-
-            if (!photonView.IsMine)
-            {
-                float lagDistance = Vector2.Distance(remotePosition, transform.position);
-                Debug.Log(lagDistance);
-                if (lagDistance> 0)
-                {
-                    transform.position = remotePosition;
-                }
-
-                            }
-            //float lag = Mathf.Abs((float)(PhotonNetwork.Time - info.timestamp));
-            //rigidbody.position += rigidbody.velocity * lag;
-        }
-    }
-
+   
 }
